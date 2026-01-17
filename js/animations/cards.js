973bc012d6e94c72336a07b25d7328af6b367bac
cloudmedata <clouddata1203@gmail.com>
2026-01-17 13:13:50 +0530

    Bug Fixes

--- js/animations/cards.js
+++ js/animations/cards.js
@@ -4,6 +4,9 @@
 
 if (window.innerWidth > 768) {
   document.querySelectorAll(".project-modern-card").forEach(card => {
+
+    card.style.willChange = "transform";
+
     card.addEventListener("mousemove", e => {
       const rect = card.getBoundingClientRect();
       const x = e.clientX - rect.left;
@@ -12,11 +15,12 @@ if (window.innerWidth > 768) {
       const rotateX = ((y / rect.height) - 0.5) * 6;
       const rotateY = ((x / rect.width) - 0.5) * -6;
 
-      card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateY(-6px)`;
+      card.style.transform =
+        `translateY(-6px) rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
     });
 
     card.addEventListener("mouseleave", () => {
-      card.style.transform = "rotateX(0) rotateY(0)";
+      card.style.transform = "translateY(0) rotateX(0) rotateY(0)";
     });
   });
 }
